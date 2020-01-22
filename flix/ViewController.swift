b0d2d62813014a4b3dd3a17bf147e276e999858f
karla <kabadtor@uci.edu>
2020-01-21 18:19:52 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Karla on 1/21/20.
+//  Copyright © 2020 Karla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
